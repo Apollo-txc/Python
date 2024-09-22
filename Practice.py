51f4bee4b23f2b492ea31785946d6abeb84e2dd3
Apollo <karonjipaul56@gmail.com>
2024-09-22 12:30:19 +0300

    Practice

---
+++ Practice.py
@@ -0,0 +1,48 @@
+#Name="Paul"
+#print("My name is "+Name+"")
+
+"""
+x=5
+i=6
+print(x+i)
+"""
+"""
+Age=22
+Add=5
+print("my new age is "+str(Age+Add))
+"""
+"""
+for i in range(1,21,2):
+    print(i)
+"""
+"""
+
+for i in range(0,21):
+    if(i%2==0):
+        print(i)
+"""
+"""
+x=int(input(" Enter a number: "))
+
+if x==0:
+    print("zero")
+elif x<0:
+    print("negative")
+else:
+    print("positive")
+"""
+"""
+X=int(input("Enter a number: "))
+if X%2==0:
+    print("Even")
+else:
+    print("Odd")
+"""
+x=int(input("Enter a X: "))
+i=int(input("Enter a I: "))
+if x>i:
+    print("x is greater than i")
+elif i>x:
+    print("i is greater than x")
+else:
+    print("x and i are equal")
